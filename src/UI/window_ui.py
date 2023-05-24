f96a2a1aaeaef13f20d39f05df8893f75cf90181
LarsSeverson <larsseverson1@gmail.com>
2023-05-23 18:17:41 -0700

    chat_box

--- src/UI/window_ui.py
+++ src/UI/window_ui.py
@@ -1,8 +1,11 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from UI.menu import Menu
+from UI.chat_ui import ChatUI
+from UI.button import *
 
 class WindowUI:
-    def __init__(self, main_window) -> None:
+    def __init__(self, main_window):
+        self.window = main_window
         self.central_widget = QtWidgets.QWidget(main_window)
         self.central_widget.setObjectName('central_widget')
         main_window.setCentralWidget(self.central_widget)
@@ -11,6 +14,10 @@ class WindowUI:
         self.height = main_window.height
 
         self.menu = Menu(self)
+        self.menu.set_chat_callback(self.open_chat)
+        self.menu.set_profile_callback(self.open_profile)
+
+        self.chat = ChatUI(self.central_widget)
 
         # self.menu_bar = QtWidgets.QMenuBar(main_window)
         # self.menu_bar.setGeometry(QtCore.QRect(0,0,992,21))
@@ -22,3 +29,10 @@ class WindowUI:
         # main_window.setStatusBar(self.status_bar)
 
         QtCore.QMetaObject.connectSlotsByName(main_window)
+
+    def open_chat(self):
+        self.chat.open()
+        print('yo')
+    
+    def open_profile(self):
+        print('profile')
