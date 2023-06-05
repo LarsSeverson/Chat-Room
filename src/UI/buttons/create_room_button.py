e9536f7c34f6d53ee5e1e94750d224bf72f8edaa
LarsSeverson <larsseverson1@gmail.com>
2023-06-05 12:59:31 -0700

    untrack __pycache__

--- src/UI/buttons/create_room_button.py
+++ src/UI/buttons/create_room_button.py
@@ -4,4 +4,11 @@ class CreateRoomButton(modules.QPushButton):
     def __init__(self):
         super().__init__()
 
-        self.setStyleSheet('background-color: rgb(227, 227, 227);')
+        self.setStyleSheet('''
+        QPushButton{
+        background-color: rgb(227, 227, 227);
+        }
+        QPushButton:hover{
+        background-color: rgb(210, 210, 210);
+        }
+        ''')
